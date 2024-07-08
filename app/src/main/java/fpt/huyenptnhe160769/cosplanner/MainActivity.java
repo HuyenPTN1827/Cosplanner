3445fba77993f8ce8fce01bb0ebfbd2182609751
HuyenPTN <huyenptn182769@gmail.com>
2024-07-08 23:35:56 +0700

    Cos List

---
+++ app/src/main/java/fpt/huyenptnhe160769/cosplanner/MainActivity.java
@@ -0,0 +1,24 @@
+package fpt.huyenptnhe160769.cosplanner;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.cos_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.CosList), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
